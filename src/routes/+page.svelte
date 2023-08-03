1e4823f12fda0c5d6afcf34ff52e32122f29bf48
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2023-08-03 22:20:21 +0700

    chore: remove unused files, add github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,17 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Svelte Chess</title>
+	<meta name="description" content="Simple chess, to learn svelte" />
 </svelte:head>
 
 <section class="h-full">
 	<Chess />
 </section>
+
+<footer class="text-white text-center mt-2">
+	<a href="https://github.com/WailanTirajoh/sveltekit-chess" class="underline text-blue-400">Go to Github</a>
+</footer>
 
 <style>
 </style>
