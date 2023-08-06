cff9bfdd15b90cc645ee5d7a82331d84f4459414
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2023-08-06 21:39:27 +0700

    chore: activate promise on view replay to simulate moving piece

--- src/routes/Chess.svelte
+++ src/routes/Chess.svelte
@@ -169,12 +169,12 @@
 			if (startPosition) {
 				setActivePiece(board[startPosition]!, startPosition);
 			}
-			// await new Promise((resolve) => setTimeout(resolve, 1000));
+			await new Promise((resolve) => setTimeout(resolve, 1000));
 			if (!finalPosition) continue;
 			if (activePiece) {
 				movePiece(activePiece, activePiece.position, finalPosition);
 			}
-			// await new Promise((resolve) => setTimeout(resolve, 1000));
+			await new Promise((resolve) => setTimeout(resolve, 1000));
 		}
 	}
 
